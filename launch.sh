1a30829cd63f064758e5cfd00d16968d9d49b96d
AlbertiPot <1071794833@qq.com>
2022-04-20 01:31:02 +0000

    add genotypes

--- launch.sh
+++ launch.sh
@@ -8,25 +8,10 @@ time=$(date "+%Y%m%d_%H%M%S")
 #     RNG_SEED 5555 > search_rot_c100_v1rotc100seed5555.log 2>&1 &
 
 # search on c100 with jig
-# CUDA_VISIBLE_DEVICES=0 nohup python -u tools/train_net.py \
+# CUDA_VISIBLE_DEVICES=3 nohup python -u tools/train_net.py \
 #     --cfg configs/search_based/search_phase/cifar100/jig.yaml \
 #     OUT_DIR result/search_jig_c100_v1jigc100seed2_${time} \
 #     RNG_SEED 2 > search_jig_c100_v1jigc100seed2.log 2>&1 &
-
-# CUDA_VISIBLE_DEVICES=1 nohup python -u tools/train_net.py \
-#     --cfg configs/search_based/search_phase/cifar100/jig.yaml \
-#     OUT_DIR result/search_jig_c100_v1jigc100seed5555_${time} \
-#     RNG_SEED 5555 > search_jig_c100_v1jigc100seed5555.log 2>&1 &
-
-# CUDA_VISIBLE_DEVICES=2 nohup python -u tools/train_net.py \
-#     --cfg configs/search_based/search_phase/cifar100/jig.yaml \
-#     OUT_DIR result/search_jig_c100_v1jigc100seed7777_${time} \
-#     RNG_SEED 7777 > search_jig_c100_v1jigc100seed7777.log 2>&1 &
-
-# CUDA_VISIBLE_DEVICES=3 nohup python -u tools/train_net.py \
-#     --cfg configs/search_based/search_phase/cifar100/jig.yaml \
-#     OUT_DIR result/search_jig_c100_v1jigc100seed9999_${time} \
-#     RNG_SEED 9999 > search_jig_c100_v1jigc100seed9999.log 2>&1 &
 
 # search on c100 with cls
 # CUDA_VISIBLE_DEVICES=1 nohup python -u tools/train_net.py \
@@ -35,13 +20,14 @@ time=$(date "+%Y%m%d_%H%M%S")
 #     RNG_SEED 9999 > search_cls_c100_v1c100clsseed9999.log 2>&1 &
 
 # eval on c100
-# CUDA_VISIBLE_DEVICES=0 nohup python -u tools/train_net.py \
-#     --cfg configs/search_based/eval_phase/cls/cifar100_custom_dartsarch.yaml \
-#     OUT_DIR result/eval_c100_v2c10org_${time} \
-#     RNG_SEED 0 > eval_c100_v2c10org.log 2>&1 &
+arch_config="v1rotc100seed7777"
+CUDA_VISIBLE_DEVICES=5 nohup python -u tools/train_net.py \
+    --cfg configs/search_based/eval_phase/cls/cifar100_custom_dartsarch.yaml \
+    OUT_DIR result/eval_c100_${arch_config}_trainseed0_${time} \
+    RNG_SEED 0 > eval_c100_${arch_config}.log 2>&1 &
 
 ################# debug
-CUDA_VISIBLE_DEVICES=1 python tools/train_net.py \
-    --cfg configs/search_based/search_phase/cifar100/jig.yaml \
-    OUT_DIR result/jig_debug \
-    RNG_SEED 9999 # > rot_debug.log 2>&1 &
+# CUDA_VISIBLE_DEVICES=1 python tools/train_net.py \
+#     --cfg configs/search_based/search_phase/cifar100/jig.yaml \
+#     OUT_DIR result/jig_debug \
+#     RNG_SEED 9999 # > rot_debug.log 2>&1 &
--- pycls/models/nas/genotypes.py
+++ pycls/models/nas/genotypes.py
@@ -631,6 +631,16 @@ UNNAS_CITYSCAPES_JIG = Genotype(
     reduce_concat=range(2, 6)
 )
 
+# rot
+v1rotc100seed2      = Genotype(normal=[('skip_connect', 0), ('dil_conv_5x5', 1), ('sep_conv_3x3', 0), ('dil_conv_3x3', 1), ('skip_connect', 0), ('dil_conv_5x5', 1), ('skip_connect', 0), ('dil_conv_3x3', 1)], normal_concat=range(2, 6), reduce=[('avg_pool_3x3', 0), ('max_pool_3x3', 1), ('avg_pool_3x3', 0), ('skip_connect', 2), ('avg_pool_3x3', 0), ('skip_connect', 2), ('sep_conv_3x3', 1), ('avg_pool_3x3', 0)], reduce_concat=range(2, 6))
+v1rotc100seed5555   = Genotype(normal=[('sep_conv_3x3', 0), ('dil_conv_5x5', 1), ('skip_connect', 0), ('dil_conv_3x3', 1), ('skip_connect', 0), ('dil_conv_5x5', 1), ('skip_connect', 0), ('dil_conv_5x5', 1)], normal_concat=range(2, 6), reduce=[('avg_pool_3x3', 1), ('avg_pool_3x3', 0), ('avg_pool_3x3', 1), ('skip_connect', 2), ('avg_pool_3x3', 0), ('skip_connect', 3), ('skip_connect', 3), ('skip_connect', 2)], reduce_concat=range(2, 6))
+v1rotc100seed7777   = Genotype(normal=[('sep_conv_3x3', 0), ('dil_conv_5x5', 1), ('skip_connect', 0), ('skip_connect', 2), ('dil_conv_3x3', 0), ('dil_conv_3x3', 1), ('skip_connect', 0), ('dil_conv_5x5', 1)], normal_concat=range(2, 6), reduce=[('sep_conv_3x3', 0), ('skip_connect', 1), ('avg_pool_3x3', 1), ('sep_conv_3x3', 0), ('dil_conv_5x5', 3), ('skip_connect', 2), ('skip_connect', 1), ('dil_conv_3x3', 0)], reduce_concat=range(2, 6))
+v1rotc100seed9999   = Genotype(normal=[('sep_conv_3x3', 0), ('dil_conv_5x5', 1), ('skip_connect', 0), ('sep_conv_3x3', 1), ('dil_conv_5x5', 0), ('dil_conv_5x5', 1), ('dil_conv_3x3', 0), ('dil_conv_5x5', 1)], normal_concat=range(2, 6), reduce=[('avg_pool_3x3', 1), ('avg_pool_3x3', 0), ('avg_pool_3x3', 1), ('skip_connect', 2), ('skip_connect', 3), ('avg_pool_3x3', 1), ('skip_connect', 0), ('skip_connect', 2)], reduce_concat=range(2, 6))
+# jig
+
+
+v1jigc100seed7777   = Genotype(normal=[('sep_conv_3x3', 1), ('sep_conv_3x3', 0), ('dil_conv_3x3', 2), ('sep_conv_3x3', 0), ('sep_conv_3x3', 1), ('sep_conv_3x3', 0), ('sep_conv_3x3', 2), ('dil_conv_3x3', 0)], normal_concat=range(2, 6), reduce=[('skip_connect', 1), ('dil_conv_3x3', 0), ('dil_conv_3x3', 2), ('max_pool_3x3', 1), ('max_pool_3x3', 1), ('sep_conv_3x3', 3), ('dil_conv_5x5', 3), ('sep_conv_5x5', 4)], reduce_concat=range(2, 6))
+v1jigc100seed9999   = Genotype(normal=[('sep_conv_3x3', 0), ('sep_conv_3x3', 1), ('sep_conv_3x3', 1), ('sep_conv_3x3', 0), ('sep_conv_3x3', 0), ('sep_conv_3x3', 1), ('dil_conv_3x3', 1), ('dil_conv_3x3', 4)], normal_concat=range(2, 6), reduce=[('dil_conv_3x3', 1), ('avg_pool_3x3', 0), ('skip_connect', 1), ('dil_conv_5x5', 2), ('dil_conv_3x3', 3), ('sep_conv_3x3', 2), ('dil_conv_5x5', 4), ('sep_conv_3x3', 1)], reduce_concat=range(2, 6))
 
 # Supported genotypes
 GENOTYPES = {
@@ -655,4 +665,11 @@ GENOTYPES = {
     'unnas_cityscapes_col': UNNAS_CITYSCAPES_COL,
     'unnas_cityscapes_jig': UNNAS_CITYSCAPES_JIG,
     'custom': None,
+    'v1rotc100seed2':v1rotc100seed2,
+    'v1rotc100seed5555':v1rotc100seed5555,
+    'v1rotc100seed7777':v1rotc100seed7777,
+    'v1rotc100seed9999':v1rotc100seed9999,
+
+    'v1jigc100seed7777':v1jigc100seed7777,
+    'v1jigc100seed9999':v1jigc100seed9999
 }
